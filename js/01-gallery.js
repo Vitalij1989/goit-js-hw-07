d43b325fe75520e7764125f1a60894dfd18e5d6d
Vitalij1989 <108751440+Vitalij1989@users.noreply.github.com>
2022-11-26 22:22:15 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const galleryMakeList = galleryItems
+  .map((item) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML("afterbegin", galleryMakeList);
+galleryContainer.addEventListener("click", onClick);
+function onClick(e) {
+  e.preventDefault();
+  const instance = basicLightbox.create(
+    `<img src=${e.target.dataset.source} />`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", onEscape);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", onEscape);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscape(e) {
+    if (e.key === "Escape") instance.close();
+  }
+}
