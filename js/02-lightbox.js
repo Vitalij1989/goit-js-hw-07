d395230436922289c0400c777ce0d1af21f9cfc4
Vitalij1989 <108751440+Vitalij1989@users.noreply.github.com>
2022-11-26 22:49:15 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const galleryMakeList = galleryItems
+  .map((item) => {
+    return `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>;`;
+  })
+  .join("");
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML("afterbegin", galleryMakeList);
+const lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
